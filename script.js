6b2882cf87976ba9381945751938308829c16f93
MariusVasie <49002727+MariusVasie@users.noreply.github.com>
2025-10-31 10:50:24 +0200

    Create the initial structure of the page.

---
+++ script.js
@@ -0,0 +1,12 @@
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`;
+    }
+}
+
+const myBook = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 180, true);
+console.log(myBook.info());
